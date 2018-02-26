ba9049a9bc9fa48407d02b541fa7930128de0ec9
pacocoursey <pvco.coursey@gmail.com>
2018-02-26 14:13:08 -0700

    init

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<div class="left">
+  <a href="#">Home</a>
+  <a href="#">Movies</a>
+</div>
+
+<div class="right">
+  <a href="#">Account</a>
+</div>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="PyFlicks...">
+    <meta name="keywords" content="movie, movies, watch, stream, recommend, movie recommend, movie recommendations">
+
+    <!-- Title -->
+    <title>PyFlicks - Find New Movies</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Favicon -->
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <link rel="manifest" href="img/manifest.json">
+    <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#292f33">
+    <meta name="theme-color" content="#292f33"> -->
+  </head>
+
+  <body>
+
+    <header>
+      <?php require_once 'includes/header.php'; ?>
+    </header>
+
+    <main>
+      <div class="movie-grid">
+        
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+        <a class="movie-item"></a>
+
+      </div>
+    </main>
+
+  </body>
+</html>
