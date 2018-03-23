440be2979565865ed8e26682be202f7cec089190
Strider102 <35611073+Strider102@users.noreply.github.com>
2018-03-23 17:09:28 -0600

    profile-templet

---
+++ profile_templet.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php require_once 'includes/head.php'; ?>
+
+    <!-- Title -->
+    <title>PyFlicks - Find New Movies</title>
+  </head>
+
+  <body>
+
+    <header>
+      <?php require_once 'includes/header.php'; ?>
+    </header>
+
+    <main>
+      <div>
+        <h4>Insert Username</h4> <br>
+        List non-private user info here <br>
+
+      </div>
+      <h4>Liked Movies:</h4><br>
+      <div class="movie-grid">
+          insert movie posters for liked movies here
+      </div>
+    </main>
+
+    <footer>
+      <?php require_once 'includes/footer.php'; ?>
+    </footer>
+
+  </body>
+</html>
