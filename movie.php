5b141cf41f8d1225f18fa2d27aea8ea65b809bfa
pacocoursey <pvco.coursey@gmail.com>
2018-03-27 12:55:39 -0600

    cleanup movie page styling

--- movie.php
+++ movie.php
@@ -16,52 +16,53 @@
     </header>
 
     <main>
-      <div>
-
-        <?php
-
-          require_once 'includes/database.php';
-          $movie_id = $_GET['id'];
-
-          if(!$movie_id || empty($movie_id)) {
-            echo 'Need to list popular movies here & also suggestions for the user.';
-          } else {
-
-            $movie = getMovieData($movie_id);
-            $produce = getProducerData($movie_id);
-
-            echo '<div class=movie-inf>';
-
-            echo '<h3>Movie Information </h3> <br>';
-            echo '<img class="movie-poster" src="images/' . $movie->poster . '">';
-            echo 'Title: ' . $movie->title . '<br>';
-            echo "Produced by: " . $produce->producer . '<br>';
-            echo 'Genre: ' . $movie->genre . '<br>';
-            echo 'Country: ' . $movie->country . '<br>';
-            echo 'Released: ' . $movie->date . '<br>';
-            echo 'Rating: ' . $movie->rating . '<br>';
-            echo "ID: ". $movie_id . '<br>';
-
-            echo "</div>";
-
-            echo "<br>";
-
-            echo "<h3>All Star Cast: </h3>";
-            echo "<div class=cast>";
 
 
-            	foreach (getActors($movie_id) as $cast) {
-            		echo '<span class=actor> ' . $cast->character . " Played by: " . $cast->actor . '</span>';
-            	}
+          <?php
 
-            echo "</div>";
+            require_once 'includes/database.php';
+            $movie_id = $_GET['id'];
+
+            if(!$movie_id || empty($movie_id)) {
+              echo '<h3>Need to list popular movies here & also suggestions for the user.</h3>';
+            } else {
+
+              $movie = getMovieData($movie_id);
+              $produce = getProducerData($movie_id);
+
+              echo '<div class="movie-block">';
+
+                echo '<h3 class="movie-header">' . $movie->title . '</h3>';
+                echo '<img class="movie-poster" src="images/' . $movie->poster . '">';
+                echo '<div class="movie-info">';
+                  echo '<p>Title: ' . $movie->title . '</p>';
+                  echo '<p>Produced by: ' . $produce->producer . '</p>';
+                  echo '<p>Genre: ' . $movie->genre . '</p>';
+                  echo '<p>Country: ' . $movie->country . '</p>';
+                  echo '<p>Released: ' . $movie->date . '</p>';
+                  echo '<p>Rating: ' . $movie->rating . '</p>';
+                  echo '<p>ID: ' . $movie_id . '</p>';
+                echo '</div>';
+
+                echo '<div class="movie-cast">';
+                  echo '<h3>Cast:</h3>';
+                  echo '<ul>';
+                    foreach(getActors($movie_id) as $cast) {
+                      echo '<li>' . $cast->actor . ': ' . $cast->character . '</li>';
+                    }
+                  echo '</ul>';
+                echo '</div>';
+
+              echo '</div>';
 
           }
 
 
-        ?>
 
-      </div>
+
+          ?>
+
+
     </main>
 
     <footer>
