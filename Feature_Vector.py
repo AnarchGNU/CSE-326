6242c3b097649dc443cc34b1e53396bc8d2cc31b
Dhrubo Saha <dhrubo@4axiz.com>
2018-04-23 16:18:30 -0600

    Recommendation Saves to database
    Finally Recommendation is saving to the database.

---
+++ Feature_Vector.py
@@ -0,0 +1,80 @@
+import time     # Used to convert release date to numeric value
+from dbConnection import dbConnection
+import pandas as pd
+import numpy as np
+
+# List of every genre that occurs in the database - should be populated immediately
+dbobj = dbConnection()
+list_genres = pd.DataFrame(dbobj.fetchAllGenre())
+list_genres = list_genres['genre_name'].values.tolist()
+
+
+### Takes a movie id and uses data from the SQL to craft a feature vector
+def mid_to_fv(movie):
+
+    #   Feature Vector:
+    #       Popularity          Float
+    #       Runtime             Integer
+    #       Vote Average        Float
+    #       Vote Count          Integer
+    #       Release Date        Integer
+    #       Genres              Boolean Array
+    #       Original Language   Boolean Array
+    #       Companies           Boolean Array
+    #       Countries           Boolean Array
+    #       Spoken Language     Boolean Array
+    #
+    #       Arrays appended to end of feature vector - not array sub-elements
+
+    # NOTE: If values are not present for a given movie, the variable is set to None
+    # list_genres = list_genres['genre_name'].values
+    # movie = pd.DataFrame(dbobj.getByMID(mid))               # Access database object
+
+    # Simple numeric values
+    runtime = float(movie['runtime'])                       # Should be set to None if not available
+    if runtime == 0:
+        runtime = None
+
+    vote_average = float(movie['avg_rating_imdb'])          # Should be set to None if not available
+    if vote_average == 0:
+        vote_average = None
+
+    vote_count = float(movie['votes_count'])                # Should be set to None if not available
+    if vote_count == 0:
+        vote_count = None
+
+    # Convert release date to numeric value (Assumes YYYY-MM-DD format)
+    release_date = ""
+    try:
+        release_date = movie['release_date']
+        rd_tup = time.strptime(release_date, '%Y-%m-%d')
+        release_date = time.mktime(rd_tup)
+    except:
+        release_date = None
+
+
+
+    # If no genre data, genres = [None] * len(list_genres)
+    # Assumes movieGenre.values returns a list of genres as string types
+    try:
+        movieGenre = movie['movie_genres'].split(",")  # Access genre list of database object
+        if len(movieGenre) == 0:
+            genres = [None] * len(list_genres)
+        else:
+            genres = [0.0] * len(list_genres)
+            for genre in movieGenre:
+                genres[list_genres.index(genre)] = 1.0
+    except:
+        genres = [None] * len(list_genres)
+
+    # Construct final feature vector
+    vec = [
+        runtime,
+        vote_average,
+        vote_count,
+        release_date
+    ]
+    vec += genres   # Append genre boolean list
+
+    # Return vector as flattened (1-dimensional) NumPy array
+    return( np.array(vec).flatten() )
---
+++ RF_Classifier.py
@@ -0,0 +1,97 @@
+import sklearn.ensemble as rf   # Random Forest
+import numpy as np              # Needed for training data
+import calendar                 # Used to time SVM training
+import time                     # Used to time SVM training
+import heapq                    # Used to rank recommendations
+
+from sklearn.preprocessing import Imputer   # Used to correct for missing data
+
+
+# Class for random forest classifier
+class RF_Clf:
+
+    ### Constructor
+    def __init__(self, iw=100):
+        # TODO: Constructor code here
+        self.input_weight = iw                  # Default weight for user preferences
+        self.RF = rf.RandomForestClassifier()   # Random Forest object
+        self.IMP = Imputer(missing_values='NaN', strategy='mean', axis=0)
+        self.t_train = None                     # Data populated after train() call
+        self.t_rank = None                      # Data populated after score_list() call
+        self.trained = False                    # Marked true in train() call
+
+    ### Training method
+    ### Expects missing values to be represented as NaN or None
+    def train(self, fg, bg):
+        train = np.array(fg + bg)
+
+        # Average missing data
+        self.IMP.fit(train)
+        train = self.IMP.transform(train)
+
+        train_weight = np.array( ([self.input_weight] * len(fg) ) + ([1] * len(bg)) )
+        cats = np.array( ([1] * len(fg)) + ([0] * len(bg)) )
+        # Cat = 1 -> Recommended (fg)
+        # Cat = 0 -> Not Recommended (bg)
+
+        # Train classifier and time training
+        t_start = calendar.timegm(time.gmtime())
+        self.RF.fit(train, cats, sample_weight=train_weight)
+        t_end = calendar.timegm(time.gmtime())
+        self.t_train = t_end - t_start
+        self.trained = True
+
+    ### Scoring method
+    ### Expects missing values to be represented as NaN or None
+    ### Checks that classifier has been trained
+    def score(self, vec):
+
+        # Check that classifier has been trained
+        assert(self.trained)
+
+        # Remove NaN data from test vector
+        test = [ np.array( vec ) ]
+        test = self.IMP.transform(test)
+
+        score = 1.0 - self.RF.predict_proba( test )[0][1]
+        return score
+
+    # def heapsort(iterable):
+    #     h = []
+    #     for value in iterable:
+    #         heapq.heappush(h, value)
+    #     return [heapq.heappop(h) for i in range(len(h))]
+
+    ### Scoring method for list of movies
+    ### Calls score()
+    ### Output = [ [score,mid] [score,mid] ... ]
+    def score_list(self, mids, vecs, mids_usr=None, out=0):
+        heap = []
+        t_start = calendar.timegm(time.gmtime())
+        for mid in mids:
+
+            fv = vecs[ mids.index(mid) ]
+            score = self.score(fv)
+            heap.append((score, mid))
+
+        heapq._heapify_max(heap)     # Rank list
+        t_end = calendar.timegm(time.gmtime())
+        self.t_rank = t_end - t_start
+
+        if (len(mids_usr) == 0) or (out == 0):
+            # Return all results
+            return heap
+
+        else:
+            # Return top results that are not in user's preferences
+            out_list = []
+            i = 0
+            while i < out:
+                out_i = heapq.heappop(heap)   # Remove top item from list
+
+                if out_i[1] not in mids_usr:
+                    mid_out = out_i[1]
+                    out_list.append(out_i)
+                    i += 1
+
+            return out_list
